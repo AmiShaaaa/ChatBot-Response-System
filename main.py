26e48b0cb39f0f43abab5b044944f65c2c195c88
Amisha <amishass2002@gmail.com>
2022-02-05 12:41:50 +0530

    added longer responses

--- main.py
+++ main.py
@@ -6,21 +6,17 @@ def message_probability(user_message, recognised_words, single_response=False, r
     message_certainty = 0
     has_required_words = True
 
-    # Counts how many words are present in each predefined message
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
-    # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
 
-    # Checks that the required words are in the string
     for word in required_words:
         if word not in user_message:
             has_required_words = False
             break
 
-    # Must either have the required words, or be a single response
     if has_required_words or single_response:
         return int(percentage * 100)
     else:
@@ -30,7 +26,6 @@ def message_probability(user_message, recognised_words, single_response=False, r
 def check_all_messages(message):
     highest_prob_list = {}
 
-    # Simplifies response creation / adds it to the dict
     def response(bot_response, list_of_words, single_response=False, required_words=[]):
         nonlocal highest_prob_list
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
@@ -53,13 +48,11 @@ def check_all_messages(message):
     return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
 
-# Used to get the response
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
     response = check_all_messages(split_message)
     return response
 
 
-# Testing the response system
 while True:
     print('Bot: ' + get_response(input('You: ')))
